f2eff447a3f963821133555edbbebe2f3dd77bae
Timothy Moores <info@tmoor.es>
2022-07-01 13:05:55 +0100

    fix setup_users

--- setup_users.sh
+++ setup_users.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/sh
 
+mkdir -p /mnesia
+mkdir -p /web
+
 adduser web-inets
+chown -R web-inets /web
+chmod -r 0744 /web
+
 adduser db-mnesia
+chown -R db-mnesia /mnesia
+chmod -r 0744 /mnesia
